dbf4fd97fb92c1e8c9c3bdd591a41074542afa3e
Jsig <jimmysignature@gmail.com>
2022-09-19 22:12:14 -0400

    Fixed Issue after Merge with Gemfile and user.rb

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    has_secure_password
+    validates :user_email, presence: true, uniqueness: true
+    validates :password, presence: true, length: {
+        minimum: 6,
+        maximum: 20
+    }
+
+end
