4fde2ee5d6f2f38d0de42c45eb14442a9a513abd
Jimmy Signature <70169816+JSignature@users.noreply.github.com>
2022-09-19 20:34:51 -0400

    Revert "Add route to create and login users"

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-    has_secure_password
-    validates :user_email, presence: true, uniqueness: true
-    validates :password, presence: true, length: {
-        minimum: 6,
-        maximum: 20
-    }
-
-end
