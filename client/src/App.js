c2d16bc8ac115796357f0cabf56bf0933131ccfb
Jimmy Andreas <jimmysignature@gmail.com>
2022-09-23 20:45:11 -0400

    change file structure add guardian search, cards, card

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,14 @@ import './App.css'
 import Home from './Home'
 // import GuardianView from './GuardianView'
 import { Route, Routes } from 'react-router-dom'
-import GuardianView from './GuardianView'
+import GuardianSearch from './GuardianSearch'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path={'/'} element={<Home />} />
-        <Route path={'/guardians'} element={<GuardianView />} />
+        <Route path={'/guardians'} element={<GuardianSearch />} />
       </Routes>
     </>
     // {/* <Home /> */}
