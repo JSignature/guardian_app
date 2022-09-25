85772018d76e8f19bf8627720716e2ddae95ace8
Jimmy Andreas <jimmysignature@gmail.com>
2022-09-25 11:41:43 -0400

    Fixed Add user issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     validates :user_email, presence: true, uniqueness: true
-    validates :password, presence: true, length: {
+    validates :password, presence: true , length: {
         minimum: 6,
         maximum: 20
     }
