e767a85b5149409815aa5a0a8d57ab1c6b7d6294
Jsig <jimmysignature@gmail.com>
2022-09-27 21:37:05 -0400

    Setup CRUD for kids

---
+++ app/controllers/kids_controller.rb
@@ -0,0 +1,60 @@
+class KidsController < ApplicationController
+
+ 
+
+
+def create
+    kid = Kid.create!(kid_params)
+
+    render json: kid, status: :created
+
+end
+
+def index
+
+ kids = Kid.all
+ render json: kids, status: :ok
+
+end
+
+def show
+    kid = Kid.find(params[:id])
+    render json: kid, status: :ok
+
+rescue ActiveRecord::RecordNotFound => error
+    render json: {message: error.message}
+end
+
+def destroy
+
+    kid = Kid.find(params[:id])
+    kid.destroy
+    head :no_content
+    
+        rescue ActiveRecord::RecordNotFound => error
+            render json: {message: error.message}
+    
+    
+    end
+    
+    def update
+    
+            kid = Kid.find(params[:id])
+            kid.update(kid_params)
+            render json: kid, status: :ok
+    
+        rescue ActiveRecord::RecordNotFound => error
+            render json: {message: error.message}
+    
+    end
+
+private
+
+def kid_params
+    params.permit(:kid_first_name, :kid_last_name, :kid_nickname, :kid_age, :kid_gender, :kid_group, :kid_allergies, :kid_notes, :kid_image, :guardian_id)
+end
+
+
+
+
+end
---
+++ app/serializers/kid_serializer.rb
@@ -0,0 +1,3 @@
+class KidSerializer < ActiveModel::Serializer
+  attributes :id, :kid_first_name, :kid_last_name, :kid_nickname, :kid_age, :kid_gender, :kid_group, :kid_allergies, :kid_notes, :kid_image, :guardian_id
+end
