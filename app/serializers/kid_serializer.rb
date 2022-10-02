be26f73d37f199b86cf9b151650b655be413883d
Jsig <jimmysignature@gmail.com>
2022-10-02 15:01:47 -0400

    Working on the activity kid link

--- app/serializers/kid_serializer.rb
+++ app/serializers/kid_serializer.rb
@@ -2,4 +2,5 @@ class KidSerializer < ActiveModel::Serializer
   attributes :id, :kid_first_name, :kid_last_name, :kid_nickname, :kid_age, :kid_gender, :kid_group, :kid_allergies, :kid_notes, :kid_image, :guardian_id
 
   belongs_to :guardian
+  has_many :activities
 end
