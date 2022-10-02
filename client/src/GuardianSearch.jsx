7ae136006b5461091c0f51fd186bcb6f1df31067
Jimmy Andreas <jimmysignature@gmail.com>
2022-10-01 22:01:24 -0400

    add style to guaridan search

--- client/src/GuardianSearch.jsx
+++ client/src/GuardianSearch.jsx
@@ -1,20 +1,21 @@
-import { React, useEffect, useState } from 'react';
-import { useGetGuardiansQuery } from './features/api/apiSlice';
-import { SearchBar } from './components/SearchBar';
-import NavBar from './components/NavBar';
-import GuardianCards from './components/GuardianCards';
-import AddGuardianModal from './components/modals/AddGuardianModal';
+import { React, useEffect, useState } from 'react'
+import { useGetGuardiansQuery } from './features/api/apiSlice'
+import { SearchBar } from './components/SearchBar'
+import NavBar from './components/NavBar'
+import GuardianCards from './components/GuardianCards'
+import AddGuardianModal from './components/modals/AddGuardianModal'
+import { DarkerBtn } from './components/styles/ButtonStyle'
 
 const GuardianSearch = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false)
 
-  const { data = [], error } = useGetGuardiansQuery();
+  const { data = [], error } = useGetGuardiansQuery()
 
   useEffect(() => {
     if (error) {
-      alert('Something Went Wrong');
+      alert('Something Went Wrong')
     }
-  }, [error]);
+  }, [error])
 
   return (
     <>
@@ -23,15 +24,14 @@ const GuardianSearch = () => {
         modalIsOpen={modalIsOpen}
         setModalIsOpen={setModalIsOpen}
       />
-      <div>
+      <div className="searchHeader">
+        <DarkerBtn onClick={() => setModalIsOpen(true)}>Add Family</DarkerBtn>
         <h1>Guardians</h1>
-        <button onClick={() => setModalIsOpen(true)}>Add Family</button>
         <SearchBar />
-
-        <GuardianCards data={data} />
       </div>
+      <GuardianCards data={data} />
     </>
-  );
-};
+  )
+}
 
-export default GuardianSearch;
+export default GuardianSearch
--- client/src/components/GuardianCard.jsx
+++ client/src/components/GuardianCard.jsx
@@ -7,30 +7,53 @@ const GuardianCard = ({ guardian }) => {
     <StyledGuardianCard>
       <img src={guardian.guardian_image} alt="" />
 
-      <footer>
+      <div>
         <h2>
           {guardian.guardian_first_name} {guardian.guardian_last_name}
         </h2>
+        <h4>{guardian.guardian_phone}</h4>
+      </div>
+      <div>
         <a href={`/guardians/${guardian.id}`}>
           <Btn>Edit Guardian</Btn>
         </a>
-      </footer>
+      </div>
     </StyledGuardianCard>
   )
 }
 
 const StyledGuardianCard = styled.article`
-  width: 90vw;
-  max-width: 300px;
-  background: white;
-  border-radius: 0.25rem;
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto 1fr auto;
+  grid-column-gap: 1.5rem;
+  margin: 1.5rem 0;
+  background-color: white;
+  box-shadow: 5px 5px 10px grey;
+  width: 70vw;
+  margin: 20px auto;
+  padding: 10px;
+  border-radius: 5px;
+
   img {
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
+    width: 5rem;
+    height: 5rem;
     object-fit: cover;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0px 0px 20px grey;
+  }
+  h2,
+  h4 {
+    margin-bottom: 0.5rem;
+    font-weight: 500;
+    letter-spacing: 2px;
+    color: var(--primary);
+    text-align: center;
+    font-size: 20px;
+  }
+  h4 {
     margin-top: 10px;
-    margin-left: 70px;
   }
 `
 
