ea082c5cd54ea46d2245cc25556512a082abe065
Jimmy Andreas <jimmysignature@gmail.com>
2022-10-01 20:59:32 -0400

    Add some styling to GuardianCard

--- client/src/components/GuardianCard.jsx
+++ client/src/components/GuardianCard.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
+import { Btn } from './styles/ButtonStyle'
 
 const GuardianCard = ({ guardian }) => {
   return (
-    <div>
-      <img style={{ width: '12rem' }} src={guardian.guardian_image} alt="" />
-      <h2>
-        {guardian.guardian_first_name} {guardian.guardian_last_name}
-      </h2>
-      <a href={`/guardians/${guardian.id}`}>
-        <button>Edit Guardian</button>
-      </a>
-    </div>
-  );
-};
+    <StyledGuardianCard>
+      <img src={guardian.guardian_image} alt="" />
 
-export default GuardianCard;
+      <footer>
+        <h2>
+          {guardian.guardian_first_name} {guardian.guardian_last_name}
+        </h2>
+        <a href={`/guardians/${guardian.id}`}>
+          <Btn>Edit Guardian</Btn>
+        </a>
+      </footer>
+    </StyledGuardianCard>
+  )
+}
+
+const StyledGuardianCard = styled.article`
+  width: 90vw;
+  max-width: 300px;
+  background: white;
+  border-radius: 0.25rem;
+  img {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    object-fit: cover;
+    margin-top: 10px;
+    margin-left: 70px;
+  }
+`
+
+export default GuardianCard
