ae38198cf6373c33c5725cc9e2c4c25171fd1658
Jimmy Andreas <jimmysignature@gmail.com>
2022-10-03 20:20:40 -0400

    add dashboard skeleton

---
+++ client/src/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+
+const Dashboard = () => {
+  return (
+    <>
+      <NavBar />
+      <h2>Hello, User!!</h2>
+      <h1>Activities</h1>
+    </>
+  )
+}
+
+export default Dashboard
