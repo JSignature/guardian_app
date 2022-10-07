7e4d16a4208c868c2f855c55bacc9219c13432ec
Jsig <jimmysignature@gmail.com>
2022-10-07 12:19:41 -0400

    Activities working with postman

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -21,8 +21,8 @@ class ActivitiesController < ApplicationController
 
     def userActivities
         # activities = Activity.where(user_id: 1)
-        activitiesByDate = Activity.where('created_at > ?', 30.days.ago)
-        activities = activitiesByDate.where(user_id: 2)
+        activitiesByDate = Activity.where('created_at > ?', 5.days.ago)
+        activities = activitiesByDate.where(user_id: 1)
 
         render json: activities,  status: :ok
 
