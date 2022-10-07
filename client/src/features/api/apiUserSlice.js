d67d2bbbfb9c518890273b59ed59560e0234c20e
Jsig <jimmysignature@gmail.com>
2022-10-07 13:25:06 -0400

    Merge in User

---
+++ client/src/features/api/apiUserSlice.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiUserSlice = createApi({
+  reducerPath: 'apiUserSlice',
+  baseQuery: fetchBaseQuery({ baseUrl: ' ' }),
+  endpoints: (builder) => ({
+    userLogin: builder.mutation({
+      query: (userObject) => {
+        return {
+          url: '/',
+          method: 'POST',
+          body: userObject,
+        };
+      },
+    }),
+  }),
+});
+
+export const { useUserLoginMutation } = apiUserSlice;
