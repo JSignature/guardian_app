3b9c7a1f4383d0f092e49d4735e93908a7492d00
Jsig <jimmysignature@gmail.com>
2022-10-08 17:32:50 -0400

    add guardian form is working needs some front end validations

--- client/src/components/modals/AddGuardianModal.jsx
+++ client/src/components/modals/AddGuardianModal.jsx
@@ -55,24 +55,70 @@ const AddGuardianModal = ({ modalIsOpen, setModalIsOpen }) => {
         <input
           type="text"
           placeholder="First Name"
-          {...register('firstName', { required: true })}
+          {...register('firstName', {
+            required: 'This is a required field',
+          })}
         />
+        <p>{errors.firstName?.message}</p>
         <input
           type="text"
           placeholder="Last Name"
-          {...register('lastName', { required: true })}
+          {...register('lastName', {
+            required: 'This is a required field',
+          })}
         />
+        <p>{errors.lastName?.message}</p>
         <input
           type="text"
           placeholder="Street Address"
-          {...register('street')}
+          {...register('street', {
+            required: 'This is a required field',
+          })}
         />
-        <input type="text" placeholder="City" {...register('city')} />
-        <input type="text" placeholder="State" {...register('state')} />
-        <input type="number" placeholder="Zip" {...register('zip')} />
-        <input type="text" placeholder="Phone" {...register('phone')} />
-        <input type="text" placeholder="Email" {...register('email')} />
+        <p>{errors.street?.message}</p>
+        <input
+          type="text"
+          placeholder="City"
+          {...register('city', {
+            required: 'This is a required field',
+            maxLength: 80,
+          })}
+        />
+        <p>{errors.city?.message}</p>
+        <input
+          type="text"
+          placeholder="State"
+          {...register('state', {
+            required: 'This is a required field',
+          })}
+        />
+        <p>{errors.state?.message}</p>
+        <input
+          type="number"
+          placeholder="Zip"
+          {...register('zip', {
+            required: 'This is a required field',
+          })}
+        />
+        <p>{errors.zip?.message}</p>
+        <input
+          type="text"
+          placeholder="Phone"
+          {...register('phone', {
+            required: 'This is a required field',
+          })}
+        />
+        <p>{errors.phone?.message}</p>
+        <input
+          type="email"
+          placeholder="Email"
+          {...register('email', {
+            required: 'This is a required field',
+          })}
+        />
+        <p>{errors.email?.message}</p>
         <input type="text" placeholder="Image" {...register('image')} />
+        <p>{errors.image?.message}</p>
         <BrighterBtn type="submit">Submit</BrighterBtn>
         {/* <input type="submit" /> */}
       </form>
