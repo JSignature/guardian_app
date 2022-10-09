444258da20b484f4b192921797067b0e8f7f713d
Jimmy Andreas <jimmysignature@gmail.com>
2022-10-09 11:47:54 -0400

    update search bar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { DarkerBtn } from './styles/ButtonStyle';
+import React from 'react'
+import { DarkerBtn } from './styles/ButtonStyle'
 
 export const SearchBar = ({ setSearchText }) => {
   return (
     <div>
-      <input type="search" onChange={(e) => setSearchText(e.target.value)} />
-      <DarkerBtn>Search</DarkerBtn>
+      <input
+        placeholder="Search by First Name"
+        type="search"
+        onChange={e => setSearchText(e.target.value)}
+      />
     </div>
-  );
-};
+  )
+}
