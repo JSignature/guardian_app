37256b8abc9e6335b0de00c4b9e1fed2ac830a9a
Jimmy Andreas <jimmysignature@gmail.com>
2022-10-16 12:05:09 -0400

    styled addMessageModal

--- client/src/components/modals/AddMessageModal.jsx
+++ client/src/components/modals/AddMessageModal.jsx
@@ -1,43 +1,48 @@
-import Modal from 'react-modal';
-import { XBtn } from '../styles/ButtonStyle';
-import { useForm } from 'react-hook-form';
-import { useAddMessageMutation } from '../../features/api/apiSlice';
-import { toast } from 'react-toastify';
+import Modal from 'react-modal'
+import { XBtn } from '../styles/ButtonStyle'
+import { useForm } from 'react-hook-form'
+import { useAddMessageMutation } from '../../features/api/apiSlice'
+import { toast } from 'react-toastify'
+import '../styles/TextModalStyle.css'
 
-Modal.setAppElement('#root');
+Modal.setAppElement('#root')
 
 const AddMessageModal = ({ modalIsOpen, setModalIsOpen, guardianPhone }) => {
-  const [sendMessage] = useAddMessageMutation();
-  const { register, handleSubmit } = useForm();
+  const [sendMessage] = useAddMessageMutation()
+  const { register, handleSubmit } = useForm()
 
-  const onSubmit = (data) => {
-    console.log(data.smsMessage);
+  const onSubmit = data => {
+    console.log(data.smsMessage)
     const message = {
       phone_number: guardianPhone,
       message: data.smsMessage,
-    };
-    sendMessage(message);
-    setModalIsOpen(false);
-    toast.success('Your Message has been sent');
-  };
+    }
+    sendMessage(message)
+    setModalIsOpen(false)
+    toast.success('Your Message has been sent')
+  }
 
   return (
     <Modal
-      ///Needs a button to close the Modal
-      className="ModalStyle"
+      className="ModalStyleText"
       isOpen={modalIsOpen}
       onRequestClose={() => setModalIsOpen(false)}
     >
       <div>
+        <h1>Text Guardian</h1>
+        <p>
+          This feature will not work in beta testing. Twilio trial account only
+          send to verified numbers.
+        </p>
         <form onSubmit={handleSubmit(onSubmit)}>
           <textarea {...register('smsMessage', {})} />
 
           <input type="submit" />
+          <button onClick={() => setModalIsOpen(false)}>X</button>
         </form>
       </div>
-      <XBtn onClick={() => setModalIsOpen(false)}>X</XBtn>
     </Modal>
-  );
-};
+  )
+}
 
-export default AddMessageModal;
+export default AddMessageModal
